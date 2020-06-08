a67701f611f92e0b206ded56e33af0db17cb8194
unknown <rocca.nicolas.e@gmail.com>
2020-06-08 04:36:26 -0300

    Finalizando

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const express=require('express');
-const gnx=require('@simtlix/gnx');
-const app=express();
+const express = require("express");
+const gnx = require("@simtlix/gnx");
+const app = express();
 
-const graphqlHTTP = require('express-graphql');
+const graphqlHTTP = require("express-graphql");
 
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-//mongoose.connect('mongodb://localhost:27017,localhost:27018,localhost:27019/example', { replicaSet: 'rs' });
-mongoose.connect('mongodb://DESKTOP-8DOUASS:27017,DESKTOP-8DOUASS:27018,DESKTOP-8DOUASS:27019/example', { replicaSet: 'rs' });
+mongoose.connect('mongodb://localhost:27017,localhost:27018,localhost:27019/example', { replicaSet: 'rs' });
 
-mongoose.connection.once('open',()=>{
-    console.log("DataBase is connected!");
+mongoose.connection.once("open", () => {
+  console.log("DataBase is connected!");
 });
 
-const types=require('./types');
-const includedTypes=Object.values(types);
-const schema=gnx.createSchema(includedTypes,includedTypes);
+const types = require("./types");
+const includedTypes = Object.values(types);
+const schema = gnx.createSchema(includedTypes, includedTypes);
 
-app.use('/graphql',graphqlHTTP({
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
-    graphiql:true
-}));
+    graphiql: true,
+  })
+);
 
-app.listen(3000,()=>{
-    console.log("App's listening on port 3000!");
+app.listen(3000, () => {
+  console.log("App's listening on port 3000!");
 });
